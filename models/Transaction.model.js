20d6cd25e7651e0f8cae6c948d1241f31aae0578
theshantanusingh <distributedservices.shan@gmail.com>
2025-09-08 07:30:29 +0000

    Feat: Added Transaction model for better logging auditing

---
+++ models/Transaction.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema(
+  {
+    accountId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Account',
+      required: true
+    },
+    type: {
+      type: String,
+      enum: ['deposit', 'withdrawal', 'transfer'],
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'completed', 'failed'],
+      default: 'completed'
+    },
+    description: String,
+  },
+  { timestamps: true }
+);
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+module.exports = Transaction;
