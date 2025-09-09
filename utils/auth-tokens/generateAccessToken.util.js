19737d17caa8e03ea858953419f2d63f830cd7fb
theshantanusingh <distributedservices.shan@gmail.com>
2025-09-09 06:29:52 +0000

    Feat: Made acess tokens and refresh token

---
+++ utils/auth-tokens/generateAccessToken.util.js
@@ -0,0 +1,9 @@
+const generateAccessToken = (user) => {
+  return jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: "15m" } 
+  );
+};
+
+module.exports = generateAccessToken;
---
+++ utils/auth-tokens/generateRefreshToken.util.js
@@ -0,0 +1,9 @@
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { id: user._id },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: "7d" } 
+  );
+};
+
+module.exports = generateRefreshToken;
