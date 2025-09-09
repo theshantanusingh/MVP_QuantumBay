cd9a965585891946eb7f249a4af5b2b9e6b89e12
theshantanusingh <distributedservices.shan@gmail.com>
2025-09-09 18:29:32 +0000

    Chores: env file updated

--- config/env.config.js
+++ config/env.config.js
@@ -1,23 +1,31 @@
-const dotenv = require('dotenv');
-const path = require('path');
+const dotenv = require("dotenv");
+dotenv.config(); // load .env file
 
-const options = require('./config.config');
-const {logger} = options;
+const logger = require("./logger.config");
 
-const required_envs = ['PORT', 'SERVICE_NAME', 'NODE_ENV', 'MONGOURI' , 'JWT_REFRESH_SECRET' , 'JWT_ACCESS_SECRET'];
+const required_envs = [
+  "PORT",
+  "SERVICE_NAME",
+  "NODE_ENV",
+  "MONGOURI",
+  "JWT_REFRESH_SECRET",
+  "JWT_ACCESS_SECRET",
+];
 
 for (const key of required_envs) {
-    if(!process.env[key]) {
-        logger.error(`Missing env variable ${key}`);
-        process.exit(1);
-    };
-};
+  if (!process.env[key]) {
+    logger.error(`❌ Missing env variable: ${key}`);
+    process.exit(1);
+  }
+}
 
 const config = {
-    nodeEnv: process.env.NODE_ENV,
-    serviceName: process.env.SERVICE_NAME,
-    port: process.env.PORT,
-    mongouri: process.env.MONGOURI
+  nodeEnv: process.env.NODE_ENV,
+  serviceName: process.env.SERVICE_NAME,
+  port: process.env.PORT,
+  mongouri: process.env.MONGOURI,
+  jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
 };
 
 module.exports = config;
